e39e5e560ccbbdf3423f834c3ef0bdce9699fb86
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-03-20 09:45:42 +0100

    first-last commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstname = prompt("scrivi il tuo nome");
+const surname = prompt("scrivi il tuo cognome");
+const color = prompt("scrivi il tuo colore preferito");
+
+const password = firstname + surname + color + "23";
+
