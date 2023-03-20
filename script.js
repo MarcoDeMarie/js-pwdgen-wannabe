0c07727678cdb93312270903128fd19d920b0ff7
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-03-20 21:24:20 +0100

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const firstname = prompt("scrivi il tuo nome");
-const surname = prompt("scrivi il tuo cognome");
-const color = prompt("scrivi il tuo colore preferito");
+const firstname = prompt ("scrivi il tuo nome");
+const surname = prompt ("scrivi il tuo cognome");
+const color = prompt ("scrivi il tuo colore preferito");
 
 const password = firstname + surname + color + "23";
 
+console.log(password);
+
+const passwordEl = document.getElementById('password');
+
+passwordEl.innerHTML= password;
